6a5d2464f1d6ccb1e7db12344a3b18cab0f0219b
adnanmi-3125 <adnanmi.business@gmail.com>
2024-08-28 15:18:00 +0530

    remove unused code

--- jest.config.ts
+++ jest.config.ts
@@ -1,20 +1,3 @@
-// import type { Config } from 'jest';
-
-// const config: Config = {
-//   testEnvironment: 'jest-environment-jsdom',
-//   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-//   transform: {
-//     '^.+\\.(ts|tsx)?$': 'ts-jest',
-//   },
-//   moduleNameMapper: {
-//     '^@/(.*)$': '<rootDir>/src/$1',
-//     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-//   },
-//   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-// };
-
-// export default config;
-
 import type { Config } from 'jest';
 
 const config: Config = {
